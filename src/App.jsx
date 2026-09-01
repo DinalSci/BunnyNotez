1c8bb2aa8496c007ca8d5bb6812f237df6046479
DinalSci <pubududinal93@gmail.com>
2026-09-01 08:59:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const MainLayout = () => {
         <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
 
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          {currentUser.role === 'admin' ? (
+          {(currentUser.role === 'admin' || currentUser.role === 'owner') ? (
             <AdminPortal />
           ) : (
             <>
